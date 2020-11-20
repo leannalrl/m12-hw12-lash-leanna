a9862be6ada477b3705aa4512929ac8abff85899
Leanna <lrlash17@ufl.edu>
2020-11-20 18:24:50 -0500

    Tried created a method using let

---
+++ main.js
@@ -0,0 +1,10 @@
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+document.getElementById("get_meal").addEventListener("click", displayMeal);
+
+function displayMeal() {
+  	let get_meal = [['Ham','hamsandwich.jpeg'], ['Pie', 'applepie.jpeg']]
+	.innerhtml = meal[1][0]
+	.innerhtml = meal[4][3]
+};
